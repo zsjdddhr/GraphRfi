cbee799f6e2599b18eab5ee4ccae89e1245b72f1
zsjdddhr <31756213+zsjdddhr@users.noreply.github.com>
2020-07-01 14:01:46 +1000

    Add files via upload

---
+++ run_experiments.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# Douban
+#python run_GraphRec_example.py -fake 0
+#python run_GraphRec_example.py -fake 0.2
+#python run_GraphRec_example.py -fake 0.4
+#python run_GraphRec_example.py -fake 0.6
+#python run_GraphRec_example.py -fake 0.8
+#python run_GraphRec_example.py -fake 1.0
+#python run_GraphRec_example.py -fake 0
+#python run_GraphRec_example.py -fake 0.2
+#python run_GraphRec_example.py -fake 0.4
+#python run_GraphRec_example.py -fake 0.6
+#python run_GraphRec_example.py -fake 0.8
+#python run_GraphRec_example.py -fake 1.0
+#python run_GraphRec_example.py -fake 0
+#python run_GraphRec_example.py -fake 0.2
+#python run_GraphRec_example.py -fake 0.4
+#python run_GraphRec_example.py -fake 0.6
+#python run_GraphRec_example.py -fake 0.8
+#python run_GraphRec_example.py -fake 1.0
+#python run_GraphRec_example.py -fake 0
+#python run_GraphRec_example.py -fake 0.2
+#python run_GraphRec_example.py -fake 0.4
+#python run_GraphRec_example.py -fake 0.6
+#python run_GraphRec_example.py -fake 0.8
+#python run_GraphRec_example.py -fake 1.0
+#python run_GraphRec_example.py -fake 0
+#python run_GraphRec_example.py -fake 0.2
+#python run_GraphRec_example.py -fake 0.4
+#python run_GraphRec_example.py -fake 0.6
+#python run_GraphRec_example.py -fake 0.8
+#python run_GraphRec_example.py -fake 1.0
+#python run_GraphRec_example.py -fake 0
+#python run_GraphRec_example.py -fake 0.2 -attack Average -dataset amazon
+#python run_GraphRec_example.py -fake 0.4 -attack Average -dataset amazon
+#python run_GraphRec_example.py -fake 0.6 -attack Average -dataset amazon
+#python run_GraphRec_example.py -fake 0.8 -attack Average -dataset amazon
+python run_GraphRec_example.py -fake 1.0 -attack Average -dataset amazon
+#python run_GraphRec_example.py -fake 0 -attack None -dataset amazon
+#python run_GraphRec_example.py -fake 1.0 -attack None -dataset amazon
+python run_GraphRec_example.py -fake 1.0 -attack Random -dataset amazon
+#python run_GraphRec_example.py -fake 1.0 -attack Hate -dataset amazon
+#python run_GraphRec_example.py -fake 0.2 -attack Average
+#python run_GraphRec_example.py -fake 0.4 -attack Average
+#python run_GraphRec_example.py -fake 0.6 -attack Average
+#python run_GraphRec_example.py -fake 0.8 -attack Average
+#python run_GraphRec_example.py -fake 1.0 -attack Average
+#python run_GraphRec_example.py -fake 0
+#python run_GraphRec_example.py -fake 0.2 -attack Average
+#python run_GraphRec_example.py -fake 0.4 -attack Average
+#python run_GraphRec_example.py -fake 0.6 -attack Average
+#python run_GraphRec_example.py -fake 0.8 -attack Average
+#python run_GraphRec_example.py -fake 1.0 -attack Average
+#python run_GraphRec_example.py -fake 0
+#python run_GraphRec_example.py -fake 0.2 -attack Random -dataset amazon
+#python run_GraphRec_example.py -fake 0.4 -attack Random -dataset amazon
+#python run_GraphRec_example.py -fake 0.6 -attack Random -dataset amazon
+#python run_GraphRec_example.py -fake 0.8 -attack Random -dataset amazon
+
+#python run_GraphRec_example.py -fake 0
+#python run_GraphRec_example.py -fake 0.2 -attack Hate -dataset amazon
+#python run_GraphRec_example.py -fake 0.4 -attack Hate -dataset amazon 
+#python run_GraphRec_example.py -fake 0.6 -attack Hate -dataset amazon
+#python run_GraphRec_example.py -fake 0.8 -attack Hate -dataset amazon
+#python run_GraphRec_example.py -fake 0.2 -attack None -dataset amazon
+#python run_GraphRec_example.py -fake 0.4 -attack None -dataset amazon
+#python run_GraphRec_example.py -fake 0.6 -attack None -dataset amazon
+#python run_GraphRec_example.py -fake 0.8 -attack None -dataset amazon
+#python run_GraphRec_example.py -fake 0
+#python run_GraphRec_example.py -fake 0.2 -attack Average
+#python run_GraphRec_example.py -fake 0.4 -attack Average
+#python run_GraphRec_example.py -fake 0.6 -attack Average
+#python run_GraphRec_example.py -fake 0.8 -attack Average
+#python run_GraphRec_example.py -fake 1.0 -attack Average
